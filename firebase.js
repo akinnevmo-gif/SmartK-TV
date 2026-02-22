d6de35105c0127e50e8ada647277979cfded1f02
akinnevmo-gif <sokpahakinsaye@gmail.com>
2026-02-22 13:09:08 +0000

    Upgrade Firebase SDK to version 10.7.1
    Updated Firebase SDK imports to version 10.7.1 and modified the firebaseConfig object to use placeholder values. Changed export variable names for storage and database.

--- firebase.js
+++ firebase.js
@@ -1,20 +1,18 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.1/firebase-app.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.6.1/firebase-storage.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.6.1/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDCei7PIT_uJDj0h5kgORxb2zzjQxq2Z00",
-  authDomain: "smartk-tv.firebaseapp.com",
-  databaseURL: "https://smartk-tv-default-rtdb.firebaseio.com",
-  projectId: "smartk-tv",
-  storageBucket: "smartk-tv.firebasestorage.app",
-  messagingSenderId: "925233991480",
-  appId: "1:925233991480:web:a231953f9f6ebffd033bcd",
-  measurementId: "G-91JZX8CYN1"
+  apiKey: "...",
+  authDomain: "...",
+  databaseURL: "...",
+  projectId: "...",
+  storageBucket: "...",
+  messagingSenderId: "...",
+  appId: "...",
 };
 
 const app = initializeApp(firebaseConfig);
-const storage = getStorage(app);
-const database = getDatabase(app);
 
-export { storage, database };
+export const storage = getStorage(app);
+export const db = getDatabase(app);
