7c99d16dd9bdcae20433f6ed87e97af08eba83ed
akinnevmo-gif <sokpahakinsaye@gmail.com>
2026-02-22 12:52:32 +0000

    Update Firebase configuration with new project details

--- firebase.js
+++ firebase.js
@@ -1,20 +1,20 @@
-// ===== Firebase Initialization =====
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.1/firebase-app.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.6.1/firebase-storage.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.6.1/firebase-database.js";
 
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    projectId: "YOUR_PROJECT",
-    storageBucket: "YOUR_PROJECT.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDCei7PIT_uJDj0h5kgORxb2zzjQxq2Z00",
+  authDomain: "smartk-tv.firebaseapp.com",
+  databaseURL: "https://smartk-tv-default-rtdb.firebaseio.com",
+  projectId: "smartk-tv",
+  storageBucket: "smartk-tv.firebasestorage.app",
+  messagingSenderId: "925233991480",
+  appId: "1:925233991480:web:a231953f9f6ebffd033bcd",
+  measurementId: "G-91JZX8CYN1"
 };
 
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
 const database = getDatabase(app);
 
-// Export for other scripts
 export { storage, database };
