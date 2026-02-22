8be609e03cda1d4f8efae4b3e3232d23aa7bae08
akinnevmo-gif <sokpahakinsaye@gmail.com>
2026-02-22 12:55:15 +0000

    Refactor addWatchLater and addFavorite functions

--- storage.js
+++ storage.js
@@ -1,29 +1,13 @@
-// ===== Storage Features =====
-
-// Save video to Watch Later
 function addWatchLater(video){
-    let list = JSON.parse(localStorage.getItem("watchLater")) || [];
-    list.push(video);
-    localStorage.setItem("watchLater", JSON.stringify(list));
-    alert("Added to Watch Later");
+  let list=JSON.parse(localStorage.getItem("watchLater"))||[];
+  list.push(video);
+  localStorage.setItem("watchLater",JSON.stringify(list));
+  alert("Added to Watch Later");
 }
 
-// Save video to Favorites
 function addFavorite(video){
-    let fav = JSON.parse(localStorage.getItem("favorites")) || [];
-    fav.push(video);
-    localStorage.setItem("favorites", JSON.stringify(fav));
-    alert("Added to Favorites");
-}
-
-// Load Watch Later list
-function showWatchLater(){
-    let list = JSON.parse(localStorage.getItem("watchLater")) || [];
-    renderList(list);
-}
-
-// Load Favorites list
-function showFavorites(){
-    let fav = JSON.parse(localStorage.getItem("favorites")) || [];
-    renderList(fav);
+  let fav=JSON.parse(localStorage.getItem("favorites"))||[];
+  fav.push(video);
+  localStorage.setItem("favorites",JSON.stringify(fav));
+  alert("Added to Favorites");
 }
